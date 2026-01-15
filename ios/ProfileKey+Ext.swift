8b8b430e54785f0b8403aed894487df58fc70095
Andrew Balmer <ab1470@users.noreply.github.com>
2026-01-15 11:11:25 -0500

    [CHNL-29884] iOS profile methods (#8)
    * implemented set email
    
    * implemented set phone number
    
    * added console messages
    
    * implemented set external ID
    
    * added helper function to convert a property key string to a `Profile.ProfileKey`
    
    * implemented `setProfileProperties`

---
+++ ios/ProfileKey+Ext.swift
@@ -0,0 +1,47 @@
+//
+//  ProfileKey+Ext.swift
+//
+//
+//  Created on 1/14/26.
+//
+
+import Foundation
+import KlaviyoSwift
+
+extension Profile.ProfileKey {
+    /// Converts a string key to a Profile.ProfileKey enum case
+    /// - Parameter key: The string key to convert
+    /// - Returns: A ProfileKey enum case, using `.custom(customKey:)` for unrecognized keys
+    static func from(_ key: String) -> Profile.ProfileKey {
+        switch key.lowercased() {
+        case "firstname", "first_name":
+            return .firstName
+        case "lastname", "last_name":
+            return .lastName
+        case "address1", "address_1":
+            return .address1
+        case "address2", "address_2":
+            return .address2
+        case "title":
+            return .title
+        case "organization":
+            return .organization
+        case "city":
+            return .city
+        case "region":
+            return .region
+        case "country":
+            return .country
+        case "zip":
+            return .zip
+        case "image":
+            return .image
+        case "latitude":
+            return .latitude
+        case "longitude":
+            return .longitude
+        default:
+            return .custom(customKey: key)
+        }
+    }
+}
