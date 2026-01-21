29ec13428cd9d6e5ba8784a8f11748569de1781a
Andrew Balmer <andrew.balmer@klaviyo.com>
2026-01-21 11:30:12 -0500

    implemented rich push

--- example/ios/NotificationServiceExtension/NotificationService.swift
+++ example/ios/NotificationServiceExtension/NotificationService.swift
@@ -5,22 +5,30 @@
 //  Created by Andrew Balmer on 1/20/26.
 //
 
+import KlaviyoSwiftExtension
 import UserNotifications
 
 class NotificationService: UNNotificationServiceExtension {
-
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    override func didReceive(
+        _ request: UNNotificationRequest,
+        withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void
+    ) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
         if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
+            // Let Klaviyo handle the notification content modification
+            // This handles:
+            // - Rich media (images) attached to push notifications
+            // - Badge count management (increment/set based on payload)
+            KlaviyoExtensionSDK.handleNotificationServiceDidReceivedRequest(
+                request: request,
+                bestAttemptContent: bestAttemptContent,
+                contentHandler: contentHandler
+            )
         }
     }
     
