921038173b47e8648804827b64c2c12e59ac8c2b
Andrew Balmer <andrew.balmer@klaviyo.com>
2026-01-21 11:30:23 -0500

    implemented  expiration handling

--- example/ios/NotificationServiceExtension/NotificationService.swift
+++ example/ios/NotificationServiceExtension/NotificationService.swift
@@ -34,10 +34,20 @@ class NotificationService: UNNotificationServiceExtension {
     
     override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
+        // Use this as an opportunity to deliver your "best attempt" at modified content,
+        // otherwise the original push payload will be used.
+        if let contentHandler = contentHandler,
+           let bestAttemptContent = bestAttemptContent
+        {
+            KlaviyoExtensionSDK.handleNotificationServiceExtensionTimeWillExpireRequest(
+                request: UNNotificationRequest(
+                    identifier: bestAttemptContent.threadIdentifier,
+                    content: bestAttemptContent,
+                    trigger: nil
+                ),
+                bestAttemptContent: bestAttemptContent,
+                contentHandler: contentHandler
+            )
         }
     }
-
 }
