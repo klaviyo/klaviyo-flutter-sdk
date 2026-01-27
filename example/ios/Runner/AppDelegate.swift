8fe35a932bf582d41de559b924f48e20f96ed0ae
Andrew Balmer <andrew.balmer@klaviyo.com>
2026-01-27 17:26:30 -0500

    simplified AppDelegate

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,4 +1,5 @@
 import Flutter
+import klaviyo_flutter_sdk
 import UIKit
 
 @main
@@ -7,7 +8,31 @@ import UIKit
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    override func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        // Show banner, sound, badge even in foreground
+        completionHandler([.banner, .sound, .badge])
+    }
+    
+    override func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        // Forward to Klaviyo for tracking
+        KlaviyoFlutterSdkPlugin.shared.userNotificationCenter(
+            center,
+            didReceive: response,
+            withCompletionHandler: completionHandler
+        )
     }
 }
